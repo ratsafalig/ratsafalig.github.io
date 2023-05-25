0959a63e78b5d1162d8a6e70651273ba63997e5c
ratsafalig <ratsafalig@outlook.com>
2023-05-25 14:18:22 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230524.211518';
+const cacheName = 'chirpy-20230525.141809';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
